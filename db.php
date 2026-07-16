9faa8b6f7ff5a7714598c74edc5304a22aa82ee8
Tanuja Deshmukh <deshmukhtanuja219@gmail.com>
2026-07-16 17:27:35 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = "rdsproject1.cboyy2qac1jk.eu-north-1.rds.amazonaws.com";
+$host = "database-1.cboyy2qac1jk.eu-north-1.rds.amazonaws.com";
 $user = "tanuja";
-$password = "rutuja54321";
+$password = "tanuja98";
 $database = "jobportal";
 
 $conn = mysqli_connect($host,$user,$password,$database);
 
 if(!$conn){
-    die("Connection Failed");
+    die("Connection Failed: " . mysqli_connect_error());
 }
 
 ?>
