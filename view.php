6278348ab5ac22c6cfd349d1cc1352943442776d
Tanuja Deshmukh <deshmukhtanuja219@gmail.com>
2026-08-24 14:31:52 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,3 +1,10 @@
+<?php
+
+include 'db.php';
+
+$sql = "SELECT * FROM candidates";
+$result = mysqli_query($conn, $sql);
+
 echo "<table border='1'>";
 
 echo "<tr>
@@ -7,6 +14,7 @@ echo "<tr>
 <th>Skill</th>
 <th>Experience</th>
 <th>Phone</th>
+<th>Resume</th>
 </tr>";
 
 while($row = mysqli_fetch_assoc($result))
@@ -18,6 +26,11 @@ while($row = mysqli_fetch_assoc($result))
     echo "<td>".$row['skill']."</td>";
     echo "<td>".$row['experience']."</td>";
     echo "<td>".$row['phone']."</td>";
+
+    echo "<td>";
+    echo "<a href='YOUR-CLOUDFRONT-URL/".$row['resume']."' target='_blank'>View Resume</a>";
+    echo "</td>";
+
     echo "</tr>";
 }
 
