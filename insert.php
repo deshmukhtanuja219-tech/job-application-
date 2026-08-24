ddbb3f3f6f36d2c549f85d96018a00c856083bff
Tanuja Deshmukh <deshmukhtanuja219@gmail.com>
2026-08-24 14:30:43 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,9 @@
 <?php
 
+require 'vendor/autoload.php';
 include 'db.php';
+
+use Aws\S3\S3Client;
 
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -8,10 +11,28 @@ $skill = $_POST['skill'];
 $experience = $_POST['experience'];
 $phone = $_POST['phone'];
 
+$resumeName = $_FILES['resume']['name'];
+$resumeTmp = $_FILES['resume']['tmp_name'];
+
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'eu-north-1'
+]);
+
+$bucket = 'YOUR-S3-BUCKET-NAME';
+
+$s3->putObject([
+    'Bucket' => $bucket,
+    'Key'    => 'resumes/' . $resumeName,
+    'SourceFile' => $resumeTmp
+]);
+
+$resumePath = 'resumes/' . $resumeName;
+
 $sql = "INSERT INTO candidates
-(name,email,skill,experience,phone)
+(name,email,skill,experience,phone,resume)
 VALUES
-('$name','$email','$skill','$experience','$phone')";
+('$name','$email','$skill','$experience','$phone','$resumePath')";
 
 if(mysqli_query($conn,$sql))
 {
