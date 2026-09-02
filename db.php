1d65048cbb1c5d121baccf16548c23a0374a4844
Tanuja Deshmukh <deshmukhtanuja219@gmail.com>
2026-09-02 14:36:47 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "job-portal1.cboyy2qac1jk.eu-north-1.rds.amazonaws.com";
-$user = "tanuja";
-$password = "tanuja73";
-$database = "jobportal1";
+$host = "endpoint";
+$user = "hostname";
+$password = "password";
+$database = "databasename";
 
 $conn = mysqli_connect($host,$user,$password,$database);
 
